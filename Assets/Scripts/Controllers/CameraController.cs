98cd2662cba080834a2f66a599716dc7f4aadf9c
p-svacha <philip.svacha@gmail.com>
2021-08-30 13:52:50 +0200

    added camera zoom change with mouse wheel

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -6,6 +6,12 @@ public class CameraController : MonoBehaviour
 {
     private Transform FocussedObject;
 
+    // Zoom
+    private float CurrentZoom = 8f;
+    private float MinZoom = 4f;
+    private float MaxZoom = 12f;
+    private float ZoomSpeed = 200f;
+
     public void FocusObject(Transform t)
     {
         FocussedObject = t;
@@ -14,6 +20,13 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.mouseScrollDelta.y != 0)
+        {
+            CurrentZoom -= Input.mouseScrollDelta.y * ZoomSpeed * Time.deltaTime;
+            CurrentZoom = Mathf.Clamp(CurrentZoom, MinZoom, MaxZoom);
+            GetComponent<Camera>().orthographicSize = CurrentZoom;
+        }
+
         if(FocussedObject != null)
         {
             transform.position = FocussedObject.transform.position + new Vector3(0, 0, -10);
